1cd3a47bf0e5a5ab723917da895047fdd79f4056
suwatchai <k_suwatchai@hotmail.com>
2019-08-09 10:01:11 +0700

    Fix readStream returns false as expected http 200 when stream event payload does not contains http header

--- src/Firebase_Arduino_WiFi101.cpp
+++ src/Firebase_Arduino_WiFi101.cpp
@@ -1,5 +1,5 @@
 /*
-* Google's Firebase Realtime Database Arduino Library for ARM/AVR WIFI Development Boards based on WiFi101 library, version 1.0.8
+* Google's Firebase Realtime Database Arduino Library for ARM/AVR WIFI Dev Boards based on WiFi101 library, version 1.0.8
 * 
 *
 * This library required WiFi101 Library to be installed.
